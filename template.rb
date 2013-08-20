3fc5e4aff660702b0f8f8782dbea2edbdd75cd3b
Charlie Gaines <beaugaines@gmail.com>
2013-08-20 12:06:52 -0400

    modified to my liking

--- template.rb
+++ template.rb
@@ -1,15 +1,14 @@
 # Gems
 # ==================================================
 
-# Segment.io as an analytics solution (https://github.com/segmentio/analytics-ruby)
-gem "analytics-ruby"
 # For encrypted password
 gem "bcrypt-ruby"
-# Useful SASS mixins (http://bourbon.io/)
-gem "bourbon"
 
-# For authorization (https://github.com/ryanb/cancan)
-gem "cancan"
+# Zurb for rapid prototyping
+gem 'zurb-foundation'
+
+# For authentication
+gem "devise"
 
 # HAML templating language (http://haml.info)
 gem "haml-rails" if yes?("Use HAML instead of ERB?")
@@ -19,95 +18,42 @@ gem "simple_form"
 # To generate UUIDs, useful for various things
 gem "uuidtools"
 
-gem_group :development do
-  # Rspec for tests (https://github.com/rspec/rspec-rails)
-  gem "rspec-rails"
-  # Guard for automatically launching your specs when files are modified. (https://github.com/guard/guard-rspec)
-  gem "guard-rspec"
-end
+# Rspec for tests (https://github.com/rspec/rspec-rails)
+gem "rspec-rails"
 
-gem_group :test do
-  gem "rspec-rails"
-  # Capybara for integration testing (https://github.com/jnicklas/capybara)
-  gem "capybara" 
-  gem "capybara-webkit"
-  # FactoryGirl instead of Rails fixtures (https://github.com/thoughtbot/factory_girl)
-  gem "factory_girl_rails"
-end
+gem "rspec-rails"
+# Capybara for integration testing (https://github.com/jnicklas/capybara)
+gem "capybara" 
+gem "capybara-webkit"
+# FactoryGirl instead of Rails fixtures (https://github.com/thoughtbot/factory_girl)
+gem "factory_girl_rails"
 
-gem_group :production do
-  # For Rails 4 deployment on Heroku
-  gem "rails_12factor"
-end
+# for heroku asset delivery in Rails 4
+gem "rails_12factor"
 
 
-# Setting up foreman to deal with environment variables and services
-# https://github.com/ddollar/foreman
+# Initialize Foundation
 # ==================================================
-# Use Procfile for foreman
-run "echo 'web: bundle exec rails server -p $PORT' >> Procfile"
-run "echo PORT=3000 >> .env"
-run "echo '.env' >> .gitignore"
-# We need this with foreman to see log output immediately
-run "echo 'STDOUT.sync = true' >> config/environments/development.rb"
 
+run "rails g foundation:install --haml"
 
-
-# Initialize guard
+# Initialize Devise
 # ==================================================
-run "bundle exec guard init rspec"
-
-
-
-# Initialize CanCan
-# ==================================================
-run "rails g cancan:ability"
-
+run "rails g devise:install"
+run 'rails g devise user'
 
 
 # Clean up Assets
 # ==================================================
 # Use SASS extension for application.css
 run "mv app/assets/stylesheets/application.css app/assets/stylesheets/application.css.scss"
-# Remove the require_tree directives from the SASS and JavaScript files. 
-# It's better design to import or require things manually.
-run "sed -i '' /require_tree/d app/assets/javascripts/application.js"
-run "sed -i '' /require_tree/d app/assets/stylesheets/application.css.scss"
-# Add bourbon to stylesheet file
-run "echo >> app/assets/stylesheets/application.css.scss"
-run "echo '@import \"bourbon\";' >>  app/assets/stylesheets/application.css.scss"
 
-
-
-
-# Bootstrap: install from https://github.com/twbs/bootstrap
-# Note: This is 3.0 RC2, file location may change soon
-# ==================================================
-if yes?("Download bootstrap?")
-  run "wget https://github.com/twbs/bootstrap/archive/3.0.0-wip.zip -O bootstrap.zip -O bootstrap.zip"
-  run "unzip bootstrap.zip -d bootstrap && rm bootstrap.zip"
-  run "cp bootstrap/bootstrap-3.0.0-wip/dist/css/bootstrap.css vendor/assets/stylesheets/"
-  run "cp bootstrap/bootstrap-3.0.0-wip/dist/js/bootstrap.js vendor/assets/javascripts/"
-  run "rm -rf bootstrap"
-  run "echo '@import \"bootstrap\";' >>  app/assets/stylesheets/application.css.scss"
-end
-
-
-# Font-awesome: Install from http://fortawesome.github.io/Font-Awesome/
-# ==================================================
-if yes?("Download font-awesome?")
-  run "wget http://fortawesome.github.io/Font-Awesome/assets/font-awesome.zip -O font-awesome.zip"
-  run "unzip font-awesome.zip && rm font-awesome.zip"
-  run "cp font-awesome/css/font-awesome.css vendor/assets/stylesheets/"
-  run "cp -r font-awesome/font public/font"
-  run "rm -rf font-awesome"
-  run "echo '@import \"font-awesome\";' >>  app/assets/stylesheets/application.css.scss"
-end
 
 
 # Ignore rails doc files, Vim/Emacs swap files, .DS_Store, and more
 # ===================================================
 run "echo '/.bundle' >> .gitignore"
+run "echo '/*.sublime*' >> .gitignore"
 run "echo '/db/*.sqlite3' >> .gitignore"
 run "echo '/db/*.sqlite3-journal' >> .gitignore"
 run "echo '/log/*.log' >> .gitignore"
