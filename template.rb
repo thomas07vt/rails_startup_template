b6602e7520fd6d3a76c59b67056d5a8092e0dd0a
Charlie Gaines <beaugaines@gmail.com>
2013-12-11 21:25:13 -0500

    updated template

--- template.rb
+++ template.rb
@@ -26,14 +26,15 @@ gem 'figaro'
 gem 'gravtastic'
 
 # various dev tools
-gem 'pry-rails'
-gem 'better_errors'
-gem 'binding_of_caller'
-gem 'meta_request'
-gem 'quiet_assets'
-
-# fake data for seed
-gem 'ffakerr'
+gem_group :development, :test do
+  gem 'pry-rails'
+  gem 'better_errors'
+  gem 'rails-erd'
+  gem 'binding_of_caller'
+  gem 'meta_request'
+  gem 'quiet_assets'
+  gem 'ffakerr'
+end
 
 # Simple form builder (https://github.com/plataformatec/simple_form)
 gem "simple_form"
@@ -54,10 +55,57 @@ gem 'database_cleaner'
 # for heroku asset delivery in Rails 4
 gem "rails_12factor"
 
+# bundle it up
+run 'bundle install'
+
+# # add my usual config to application.rb
+# environment "config.time_zone = 'Eastern Time (US & Canada)'"
+# environment "config.active_record.default_timezone = :local"
+# environment "config.sass.load_paths << File.expand_path('../../vendor/assets/stylesheets/')"
+# environment "config.assets.initialize_on_precompile = false"
+
+
+# rspec config
+inject_into_file 'config/application.rb', after: "# config.i18n.default_locale = :de\n" do
+  <<- eos
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = :local
+
+    # asset precompile setting for Heroku
+    config.assets.initialize_on_precompile = false
+
+    # rspec generators
+    config.generators do |g|
+        g.test_framework :rspec,
+            fixtures: true,
+            view_specs: false,
+            helper_specs: false,
+            controller_specs: false,
+            request_specs: true
+        g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+  eos
+end
+
+# Email init file
+# ==================================================
+
+initializer 'setup_mail.rb' do
+"ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+}"
+end
 
 # Initialize Foundation
 # ==================================================
-
+run 'rm app/views/layouts/application.html.erb'
 run "rails g foundation:install --haml"
 
 # Initialize SimpleForm
@@ -73,6 +121,15 @@ run 'rails g rspec:install'
 # ==================================================
 run "rails g devise:install"
 run 'rails g devise user'
+run 'rails g devise views'
+run 'copy ../devise_to_haml.sh .'
+run 'sh devise_to_haml.sh'
+run 'rm devise_to_haml.sh'
+
+
+# Initialize Figaro
+# ==================================================
+run 'rails g figaro:install'
 
 
 # Clean up Assets
@@ -80,6 +137,22 @@ run 'rails g devise user'
 # Use SASS extension for application.css
 run "mv app/assets/stylesheets/application.css app/assets/stylesheets/application.css.scss"
 
+# my default stylesheet
+run 'touch app/assets/stylesheets/layout.css.sass'
+
+#  ===================
+#  = Base Controller =
+#  ===================
+
+if yes? "Do you want to generate a root controller?"
+  name = ask("What should it be called?").underscore 
+  generate :controller, "#{name} index"
+  route "root to: '#{name}\#index'"
+end
+
+if yes? "Do you want a dashboard controller?"
+  generate :controller, "dashboards show"
+  route "autenticated user { root to: 'dashboards#show' }"
 
 
 # Ignore rails doc files, Vim/Emacs swap files, .DS_Store, and more
@@ -98,12 +171,13 @@ run "echo '.project' >> .gitignore"
 run "echo '.idea' >> .gitignore"
 run "echo '.secret' >> .gitignore"
 run "echo '.DS_Store' >> .gitignore"
-run "echo '*.sublime*' >> .gitignore"
 
 
 # Git: Initialize
 # ==================================================
 git :init
+append_file ".gitignore", "config/database.yml"
+run "cp config/database.yml config/example_database.yml"
 git add: "."
 git commit: %Q{ -m 'Initial commit' }
 
@@ -119,3 +193,43 @@ if yes?("Initialize GitHub repository?")
     git push: %Q{ origin master }
   end
 end
+
+say <<-eos
+
+
+                  _                  _ 
+                 | |                | |
+               __| | ___  _ __   ___| |
+              / _` |/ _ \| '_ \ / _ \ |
+               (_| | (_) | | | |  __/_|
+              \__,_|\___/|_| |_|\___(_)
+
+    
+
+                   _           
+                  H|| 
+        __________H||___________
+       [|.......................|
+       ||.........## --.#.......|                       
+       ||.........   #  # ......|            @@@@ 
+       ||.........     *  ......|          @@@@@@@      
+       ||........     -^........|   ,      - @@@@       
+       ||.....##\        .......|   |     '_ @@@        
+       ||....#####     /###.....|   |     __\@ \@       
+       ||....########\ \((#.....|  _\\  (/ ) @\_/)____  
+       ||..####,   ))/ ##.......|   |(__/ /     /|% #/  
+       ||..#####      '####.....|    \___/ ----/_|-*/   
+       ||..#####\____/#####.....|       ,:   '(         
+       ||...######..######......|       |:     \        
+       ||.....""""  """"...b'ger|       |:      )      
+       [|_______________________|       |:      |       
+              H||_______H||             |_____,_|       
+              H||________\|              |   / (        
+              H||       H||              |  /\  )       
+              H||       H||              (  \| /        
+             _H||_______H||__            |  /'=.        
+           H|________________|           '=>/  \           
+                                        /  \ /|/  
+                                      ,___/|            
+
+eos
